3574e28d61e3f5540147712c389b62d4bc5bff33
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2021-09-11 18:46:22 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,70 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Seller</th>
+                        <th>Customers Visiteds</th>
+                        <th>Closed Deals</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Barry Allen</td>
+                        <td>32</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por Gabriel Augusto</p>
+                <p className="text-light">Github: <a href="https://github.com/gabrielfreitas3110" target="_blank" rel="noreferrer">gabrielfreitas3110</a></p>
+                <p className="text-light">LinkedIn: <a href="https://www.linkedin.com/in/gabriel-augusto3110/" target="_blank" rel="noreferrer">gabriel-augusto3110</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

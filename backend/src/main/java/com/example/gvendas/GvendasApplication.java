dd55f368be8649045baf417fda39b8007ecd3f4b
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2021-09-11 13:14:01 -0300

    Project created

---
+++ backend/src/main/java/com/example/gvendas/GvendasApplication.java
@@ -0,0 +1,13 @@
+package com.example.gvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/gvendas/GvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

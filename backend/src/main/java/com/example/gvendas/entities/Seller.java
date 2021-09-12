8241532cb64bcbb6a0541ed373e15871135623c4
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2021-09-12 01:32:55 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/example/gvendas/entities/Seller.java
@@ -0,0 +1,60 @@
+package com.example.gvendas.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> salers = new ArrayList<>();
+
+    public Seller() {
+    }
+
+    public Seller(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Sale> getSales() {
+        return salers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

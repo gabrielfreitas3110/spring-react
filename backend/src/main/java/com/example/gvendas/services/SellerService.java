585b55e0070a5448cc7dac87e25c0e0f672b3632
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2021-09-12 02:06:45 -0300

    Layers

---
+++ backend/src/main/java/com/example/gvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.example.gvendas.services;
+
+import com.example.gvendas.dto.SellerDTO;
+import com.example.gvendas.entities.Seller;
+import com.example.gvendas.repositories.SellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class SellerService {
+
+    @Autowired
+    private SellerRepository repository;
+
+    public List<SellerDTO> findAll() {
+        List<Seller> result = repository.findAll();
+        return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+    }
+}
